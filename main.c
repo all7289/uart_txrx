91619b48208a644f030865de1add0d0b29459763
all7289 <56290632+all7289@users.noreply.github.com>
2022-05-25 00:40:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -21,25 +21,56 @@ void uart2_write(int ch);
 char uart2_read(void);
 void delayMs(int delay);
 
+void dot(void);
+void dash(void);
+void SoundLetter(char text);
+
 char key;
+uint8_t status;
+
 
 int main(void)
 {
+	// clock
+		RCC->AHBENR |= 0x40000;
+		//define GPIOB_3
+		GPIOB->MODER |=0x40;
+		GPIOB->OSPEEDR |=0xC0;
+		//GPIOB->PUPDR |=0x40;
+		//GPIOB_6
+		GPIOB->MODER |=0x1000;
+		GPIOB->OSPEEDR |=0x3000;
+
 	uart2_rxtx_init();
 
+	GPIOB->BSRR |=0x40;//status 1 at pinB6
+	GPIOB->BSRR |=0x8;//status 1 at pinB3
   while (1)
   {
+	 // uart2_write('Q');
 	  key= uart2_read();
-	  	  if (key == 'a')
-	  	  {
-	  		  GPIOB->BSRR |=0x8;//led on
-	  		  	  delayMs(1000);
-	  	  }
-	  	  else
-	  	  {
-	  		  GPIOB->BSRR |=0x80000;//led off
-	  		  	  delayMs(1000);
-	  	  }
+
+	  uart2_write(key);
+	  SoundLetter(key);
+
+//	  	  if (key == '1')
+//	  	  {
+////	  		  GPIOB->BSRR |=0x8;//led on
+////	  		  	  delayMs(100);
+//
+//	  		  dot();
+//	  		  dot();
+//	  	  }
+//	  	  else
+//	  	  {
+////	  		  GPIOB->BSRR |=0x80000;//led off
+////	  		  	  delayMs(100);
+//	  		  dash();
+//	  		  dash();
+//	  		SoundLetter(key);
+//	  	  }
+
+
   }
 }
 
@@ -61,7 +92,7 @@ void uart2_rxtx_init(void)
 
 	/***************Configre module uart Rx ***************/
 
-		USART2->CR1 |= 0x4;//configure transfer direcction
+		USART2->CR1 |= 0x4;//configure receiver direcction
 		USART2->CR1 |=0x1;//enable mode uart
 
 }
@@ -97,5 +128,174 @@ char uart2_read(void)
 	{
 	}
 	return USART2->RDR;
+}
+
+/**********kur project*********/
+void dot(void)
+{
+	//HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6, 0);
+	GPIOB->BSRR |=0x400000;
+	delayMs(50);
+	//HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6, 1);
+	GPIOB->BSRR |=0x40;
+	delayMs(200);
+	//HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 1);
+	//GPIOB->BSRR |=0x8;
+}
+
+void dash(void)
+{
+	//HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6, 0);
+	GPIOB->BSRR |=0x400000;
+	delayMs(200);
+	//HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6, 1);
+	GPIOB->BSRR |=0x40;
+	delayMs(200);
+	//HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6, 1);
+	//GPIOB->BSRR |=0x8;
+}
+
+void SoundLetter(char text)
+{
+  switch(text)
+  {
+    case 'E':
+      dot();
+      return;
+    case 'T':
+      dash();
+      return;
+    case 'A':
+      dot();
+      dash();
+      return;
+    case 'O':
+      dash();
+      dash();
+      dash();
+      return;
+    case 'I':
+      dot();
+      dot();
+      return;
+    case 'N':
+      dash();
+      dot();
+      return;
+    case 'S':
+      dot();
+      dot();
+      dot();
+      return;
+    case 'H':
+      dot();
+      dot();
+      dot();
+      dot();
+      return;
+    case 'R':
+      dot();
+      dash();
+      dot();
+      return;
+    case 'D':
+      dash();
+      dot();
+      dot();
+      return;
+    case 'L':
+      dot();
+      dash();
+      dot();
+      dot();
+      return;
+    case 'C':
+      dash();
+      dot();
+      dash();
+      dot();
+      return;
+    case 'U':
+      dot();
+      dot();
+      dash();
+      return;
+    case 'M':
+      dash();
+      dash();
+      return;
+    case 'W':
+      dot();
+      dash();
+      dash();
+      return;
+    case 'F':
+      dot();
+      dot();
+      dash();
+      dot();
+      return;
+    case 'G':
+      dash();
+      dash();
+      dot();
+      return;
+    case 'Y':
+      dash();
+      dot();
+      dash();
+      dash();
+      return;
+    case 'P':
+      dot();
+      dash();
+      dash();
+      dot();
+      return;
+    case 'B':
+      dash();
+      dot();
+      dot();
+      dot();
+      return;
+    case 'V':
+      dot();
+      dot();
+      dot();
+      dash();
+      return;
+    case 'K':
+      dash();
+      dot();
+      dash();
+      return;
+    case 'J':
+      dot();
+      dash();
+      dash();
+      dash();
+      return;
+    case 'X':
+      dash();
+      dot();
+      dot();
+      dash();
+      return;
+    case 'Q':
+      dash();
+      dash();
+      dot();
+      dash();
+      return;
+    case 'Z':
+      dash();
+      dash();
+      dot();
+      dot();
+      return;
+    case ' ':
+    	delayMs(400);
+      return;
+  }
 }
 
